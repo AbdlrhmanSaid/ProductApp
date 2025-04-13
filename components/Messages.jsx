e44c52b4fa6710b5aee4950837aa34fc11d92d43
Abdelrhman Said <119136719+AbdlrhmanSaid@users.noreply.github.com>
2025-04-13 13:31:21 +0200

    Update header text in Messages component for localization

--- components/Messages.jsx
+++ components/Messages.jsx
@@ -16,7 +16,7 @@ const Messages = () => {
       className="container mx-auto mt-5 bg-white p-6 rounded-lg shadow-lg"
     >
       <h1 className="text-3xl font-extrabold mb-6 text-center text-[#1976D2]">
-        التفاصيل
+        الاشعارات
       </h1>
 
       {loading ? (
